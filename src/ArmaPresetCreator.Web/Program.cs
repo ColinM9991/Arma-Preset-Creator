b239a6b88180da4fe889127d3266ff979c7876d7
ColinM9991 <57466042+ColinM9991@users.noreply.github.com>
2022-12-14 22:07:47 +0000

    Add environment variable support

--- src/ArmaPresetCreator.Web/Program.cs
+++ src/ArmaPresetCreator.Web/Program.cs
@@ -1,5 +1,6 @@
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Serilog;
 
 namespace ArmaPresetCreator.Web
@@ -11,12 +12,18 @@ namespace ArmaPresetCreator.Web
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                    .UseSerilog((hostingContext, loggerConfiguration) => 
-                        loggerConfiguration
-                        .ReadFrom.Configuration(hostingContext.Configuration)
-                        .Enrich.FromLogContext());
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webHostBuilder =>
+                {
+                    webHostBuilder
+                        .UseStartup<Startup>()
+                        .ConfigureAppConfiguration(configuration => configuration.AddEnvironmentVariables())
+                        .UseSerilog((hostingContext, loggerConfiguration) =>
+                            loggerConfiguration
+                                .ReadFrom.Configuration(hostingContext.Configuration)
+                                .Enrich.FromLogContext());
+                });
+
     }
 }
