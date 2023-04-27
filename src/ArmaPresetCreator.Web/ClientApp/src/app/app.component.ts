c1a48c71475bd3203b5b2975f8721010c6dd23bf
ColinM9991 <57466042+ColinM9991@users.noreply.github.com>
2023-04-27 23:42:08 +0100

    Support bootstrap 5.3 dark mode

--- src/ArmaPresetCreator.Web/ClientApp/src/app/app.component.ts
+++ src/ArmaPresetCreator.Web/ClientApp/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   date = new Date();
+
+  constructor(private renderer: Renderer2) {
+  }
+
+  onThemeChange(event: any) {
+    let newTheme = event.target.value;
+    this.changeTheme(newTheme);
+  }
+
+  private changeTheme(themeName: string) {
+    this.renderer.setAttribute(document.body, "data-bs-theme", themeName);
+  }
+
+  ngAfterViewInit(): void {
+    this.changeTheme("dark");
+  }
 }
